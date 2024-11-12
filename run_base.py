d6c976f8d541aeccd50212f873b184a43e69da5d
chenglongzu <chenglongzu@gmail.com>
2024-11-12 22:25:56 +0800

    fix: 加入Torch模型加载CPU的支持

--- run_base.py
+++ run_base.py
@@ -100,7 +100,7 @@ def run(rank, num_procs, args):
     if (trained_model_path != ""):
         try:
             model = model_builder.build_model(model_config_file, dataset, device)
-            state_dict = torch.load(trained_model_path)
+            state_dict = torch.load(trained_model_path, map_location=torch.device(device))
             model.load_state_dict(state_dict)
         except:
             model = torch.load(trained_model_path)
